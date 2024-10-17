62fe2372a192c1ad3b17ad4de224aedb4c7ccc7b
Comry <ComryOnWeb@foxotic.git.io>
2024-10-17 10:21:45 +0200

    Image dice.png

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     const diceImage = document.getElementById('dice');
 
     // Обновляем картинку кубика в зависимости от выпавшего значения
-    diceImage.src = `images/dice${diceValue}.png`;
+    diceImage.src = `dice${diceValue}.png`;
 
     // Также можно обновить текст с результатом (необязательно)
     document.getElementById('result').innerHTML = `Результат: ${diceValue}`;
