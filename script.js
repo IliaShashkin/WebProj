f45dbb57a933269967b754a9e1859dd13ba1a3d9
Comry <ComryOnWeb@foxotic.git.io>
2024-10-21 08:14:21 +0200

    no script file

--- script.js
+++
@@ -1,9 +0,0 @@
-﻿function rollDice() {
-    const diceValue = Math.floor(Math.random() * 20) + 1;
-    const diceImage = document.getElementById('dice');
-
-    diceImage.src = `dice${diceValue}.png`;
-
-
-    document.getElementById('result').innerHTML = `Result: ${diceValue}`;
-}
