c705274ada5e31b643f83d26204ea4dca068c069
Comry <ComryOnWeb@foxotic.git.io>
2024-10-21 08:02:54 +0200

    eNGLISH

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
     const diceValue = Math.floor(Math.random() * 20) + 1;
     const diceImage = document.getElementById('dice');
 
-    // Обновляем картинку кубика в зависимости от выпавшего значения
     diceImage.src = `dice${diceValue}.png`;
 
-    // Также можно обновить текст с результатом (необязательно)
+
     document.getElementById('result').innerHTML = `Результат: ${diceValue}`;
 }
